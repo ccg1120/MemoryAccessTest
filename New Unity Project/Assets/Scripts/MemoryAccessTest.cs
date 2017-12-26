fe7691cd25a47cb057ddc0d4d9b5bb9c4db12d1d
ccg1120 <ccg.1120@daum.net>
2017-12-27 01:07:51 +0900

    메모리 접근 방식에 따른 속도 비교 중

---
+++ New Unity Project/Assets/Scripts/MemoryAccessTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Timers;
+using System.Diagnostics;
+
+/// <summary>
+/// C# 배열과 동적배열에서의 메모리 접근 속도 테스트 
+/// </summary>
+public class MemoryAccessTest : MonoBehaviour {
+
+    //public int[] intarry = new int[100000000];
+
+    public int[] intarry2;
+
+    
+
+
+	// Use this for initialization
+	void Start () {
+        //intarry2 = new int[100000000];
+       
+        
+
+        Stopwatch sw = new Stopwatch();
+
+        sw.Reset();
+        sw.Start();
+        
+        //for (int index = 0; index < intarry2.Length; index++)
+        //{
+        //    intarry2[index] = 0;
+        //}
+
+        unsafe
+        {
+            fixed (int* pintarry = new int[100000000])
+            {
+                for (int index = 0; index < 100000000; index++)
+                {
+                    pintarry[index] = 0;
+                }
+            }
+        }
+        sw.Stop();
+        UnityEngine.Debug.Log(sw.Elapsed.ToString());
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
